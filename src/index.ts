1489505c5d6420c0ece1803eae29641fbff21537
Archie <76575196+archiewang0@users.noreply.github.com>
2025-02-27 23:52:29 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './app';
-import db from './models';
+// import db from './models';
 import http from 'http';
 
 import config from "./config";
@@ -7,10 +7,12 @@ import config from "./config";
 const port = config.port || 8080;
 const server: http.Server = http.createServer(app);
 
-db.sequelize.sync().then(() => {
-    server.listen(port, () => {
-        console.log(`App listening on port ${port}`);
-    });
+// db.sequelize.sync().then(() => {
+  
+// });
+
+server.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 });
 
 module.exports = app;
